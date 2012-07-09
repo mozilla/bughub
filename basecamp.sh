ace1c426d69b07a62b13e8b45aeb6f1686f9ae17
Carl Meyer <carl@oddbird.net>
2012-07-09 11:57:15 -0600

    Actually get all github issues.

--- basecamp.sh
+++ basecamp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python bughub.py -v bugzilla:cf_blocking_basecamp=+ github:mozilla-b2g:gaia:state=open,closed
+python bughub.py -v bugzilla:cf_blocking_basecamp=+ github:mozilla-b2g:gaia:state=open github:mozilla-b2g:gaia:state=closed
